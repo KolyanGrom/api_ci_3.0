d64b1d952eeb69483add402e81444487b2845c3e
KolyanGrom <kolyangrom@yandex.ru>
2024-04-07 21:39:14 +0700

    added project

---
+++ src/test/java/ru/netology/test/JsonTest.java
@@ -0,0 +1,22 @@
+package ru.netology.test;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class JsonTest {
+    @Test
+    void DemoTest() {
+    // Given - When - Then
+// Предусловия
+    given()
+  .baseUri("https://postman-echo.com")
+  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+.when()
+  .post("/post")
+// Проверки
+.then()
+  .statusCode(200);
+  //.body(/* --> ваша проверка здесь <-- */);
+}}
