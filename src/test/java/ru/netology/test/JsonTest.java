409be718ff39e38ea1464d0d567a27b146c06c2b
KolyanGrom <kolyangrom@yandex.ru>
2024-04-07 23:30:59 +0700

    add test

--- src/test/java/ru/netology/test/JsonTest.java
+++ src/test/java/ru/netology/test/JsonTest.java
@@ -3,20 +3,22 @@ package ru.netology.test;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class JsonTest {
     @Test
     void DemoTest() {
-    // Given - When - Then
+        // Given - When - Then
 // Предусловия
-    given()
-  .baseUri("https://postman-echo.com")
-  .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
-.when()
-  .post("/post")
+                .when()
+                .post("/post")
 // Проверки
-.then()
-  .statusCode(200);
-  //.body(/* --> ваша проверка здесь <-- */);
-}}
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+    }
+}
